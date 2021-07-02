63737990028c9a5bfb2102787f329538f79c4752
martin <jmcnualan@multisyscorp.com>
2021-07-02 14:50:55 +0800

    first

---
+++ src/Application.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Dmn\Cmn;
+
+use Carbon\Carbon;
+use Laravel\Lumen\Application as LumenApplication;
+
+class Application extends LumenApplication
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function version()
+    {
+        return @str_replace(
+            "\n",
+            '',
+            file_get_contents(base_path('version'))
+        );
+    }
+
+    /**
+     * Get app information
+     *
+     * @return void
+     */
+    public function information()
+    {
+        return [
+            'name' => config('app.name'),
+            'version' => $this->version(),
+            'timezone' => config('app.timezone'),
+            'timestamp' => new Carbon(),
+            'client_ip' => request()->ip(),
+        ];
+    }
+}
