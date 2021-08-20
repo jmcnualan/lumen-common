b9146f78155cc907e0e301453b4508c9a667c9a2
martin <jmcnualan@multisyscorp.com>
2021-08-20 12:02:59 +0800

    Add perpage to base controller

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -8,6 +8,27 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     /**
+     * Get per page
+     *
+     * @param int $limit
+     * @param int $maxPerPage
+     *
+     * @return int
+     */
+    protected function getPerPage(int $limit = 50, int $maxPerPage = 100): int
+    {
+        $default = config('pagination.default_per_page', $limit);
+
+        $perPage = request()->get('per_page', $default);
+
+        if ($perPage > $maxPerPage) {
+            $perPage = $maxPerPage;
+        }
+
+        return $perPage;
+    }
+
+    /**
      * No content
      *
      * @return Response
